a5db09820d2d7ede0dff65ab5078a0d358fe36de
Zafiku <diegotorresortiz@hotmail.es>
2014-10-08 18:55:41 +0200

    g

--- Assets/Scripts/Gema.cs
+++ Assets/Scripts/Gema.cs
@@ -2,14 +2,9 @@
 using System.Collections;
 
 public class Gema : MonoBehaviour {
+	{
+		void OnTriggerEnter2D(Collider2D other); }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	public void Reiniciar (){
+		Application.LoadLevel ("menu"); }
 }
